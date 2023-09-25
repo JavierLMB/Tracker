44a55b61f83af864ecfdbc1d9b28ac69797f813b
JavierLMB~ <javier.camilleri29@gmail.com>
2023-09-25 13:10:47 +0200

    "Content added w/o styling"

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,101 @@
 import { useState } from "react";
+import maleAvatar from "./images/maleAvatar.svg";
 
-function App() {
-  return <div></div>;
+const initialData = [
+  {
+    type: "Work",
+    hours: 32,
+    lastWeek: 36,
+  },
+  {
+    type: "Play",
+    hours: 10,
+    lastWeek: 8,
+  },
+  {
+    type: "Study",
+    hours: 4,
+    lastWeek: 7,
+  },
+  {
+    type: "Exercise",
+    hours: 4,
+    lastWeek: 5,
+  },
+  {
+    type: "Social",
+    hours: 5,
+    lastWeek: 10,
+  },
+  {
+    type: "Self Care",
+    hours: 2,
+    lastWeek: 2,
+  },
+];
+
+export default function App() {
+  return (
+    <div>
+      <Tracker />
+    </div>
+  );
 }
 
-export default App;
+function Tracker() {
+  return (
+    <div>
+      <Profile />
+      <CardList />
+    </div>
+  );
+}
+
+function Profile() {
+  return (
+    <div>
+      <UserInfo />
+      <DatePreview />
+    </div>
+  );
+}
+
+function UserInfo() {
+  return (
+    <div>
+      <img className="profileIcon" src={maleAvatar} alt="Avatar" />
+      <p>Report for</p>
+      <h3>Jeremy Robson</h3>
+    </div>
+  );
+}
+
+function DatePreview() {
+  return (
+    <ul>
+      <li>Daily</li>
+      <li>Weekly</li>
+      <li>Monthly</li>
+    </ul>
+  );
+}
+
+function CardList() {
+  return (
+    <ul>
+      {initialData.map((data) => (
+        <Card data={data} key={data.type} />
+      ))}
+    </ul>
+  );
+}
+
+function Card({ data }) {
+  return (
+    <li>
+      <h3>{data.type}</h3>
+      <p>{data.hours}</p>
+      <p>{data.lastWeek}</p>
+    </li>
+  );
+}
